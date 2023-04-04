c8ecd7ffafce47ea2b9f070b4c8d0920c666548a
Ryan Derr <54389874+RyanDerr@users.noreply.github.com>
2023-04-03 21:31:34 -0500

    Start Develop On Golang Simple Implementation

---
+++ Golang/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		//Print a '>' to the console before entering commands
+		fmt.Print("> ")
+		//Read Input From Keyboard
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err = executeCommand(input); err != nil {
+			fmt.Fprintln(os.Stdout, err)
+		}
+	}
+}
+
+func executeCommand(commandInput string) error {
+	commandInput = strings.TrimSuffix(commandInput, "\n")
+
+	cmd := exec.Command(commandInput)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
